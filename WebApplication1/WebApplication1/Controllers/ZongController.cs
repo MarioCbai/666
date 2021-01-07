a5fddb7a2bdda8d051c8586f3bc472bd20d53507
2282143659@qq.com <2282143659@qq.com>
2021-01-07 10:08:29 +0800

    1

---
+++ WebApplication1/WebApplication1/Controllers/ZongController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ZongController : Controller
+    {
+        // GET: Zong
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
